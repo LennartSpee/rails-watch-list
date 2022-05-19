1d73764435e3cebb4c98cacf25f243050b1c6282
Lennart Spee <lennart@digitallokal.de>
2022-05-19 14:35:43 +0100

    validations done & seed feed

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -3,5 +3,5 @@ class Bookmark < ApplicationRecord
   belongs_to :list
 
   validates :comment, presence: true, length: { minimum: 6 }
-  validates [movie, list], uniqueness: true
+  validates_uniqueness_of :movie, scope: :list
 end
